338cc5d8d86d63ced1259fb76ddda0065ce846fc
n.vaneeva <n.vaneeva@keep-calm.ru>
2023-09-03 15:42:52 +0400

    getting data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useState, useEffect } from "react"
+import axios from "axios"
+import Todo from "./components/Task";
+
+export interface ITodo {title: string; userId: number; id: number; completed: boolean}
+
+const src = 'https://jsonplaceholder.typicode.com/todos'
 
 function App() {
+  const [todos, setTodos] = useState<ITodo[]>([])
+
+  useEffect(() => {
+    axios.get(src)
+    .then((data) => setTodos(data.data))
+  }, [])
 
   return (
     <>
-    Hello
+    {todos.map((item) => <Todo todo={item}/>)}
     </>
   )
 }
