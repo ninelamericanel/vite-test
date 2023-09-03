79aa00f7bbd4205c128a9cdde7c784e78e807acc
n.vaneeva <n.vaneeva@keep-calm.ru>
2023-09-03 14:20:03 +0400

    create global styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createGlobalStyle } from 'styled-components'
 import App from './App.tsx'
+
+const Global = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: -apple-system, 'system-ui', 'BlinkMacSystemFont', 'Segoe UI', 'Noto Sans', Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji';
+  }
+
+  li {
+    list-style: none;
+  }
+`
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <Global />
     <App />
   </React.StrictMode>,
 )
